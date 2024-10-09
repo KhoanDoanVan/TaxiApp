544bc38121345517b15e45f5ed8ffb0b95ee4cfb
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-10-09 17:22:22 +0700

    Initial Commit

---
+++ TaxiApp/TaxiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiAppApp.swift
+//  TaxiApp
+//
+//  Created by Đoàn Văn Khoan on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TaxiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
